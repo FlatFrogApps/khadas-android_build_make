4cc7242824b14b877f75beabd3774fca90f5b7a5
goenjoy <goenjoy@khadas.com>
2019-07-16 10:37:31 +0800

    remove Browser2,Contacts,Email and QuickSearchBox app

--- target/product/core.mk
+++ target/product/core.mk
@@ -24,17 +24,14 @@ PRODUCT_PACKAGES += \
     BasicDreams \
     BlockedNumberProvider \
     BookmarkProvider \
-    Browser2 \
     BuiltInPrintService \
     Calendar \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
-    Contacts \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
     ExactCalculator \
     ExternalStorageProvider \
     FusedLocation \
@@ -49,7 +46,6 @@ PRODUCT_PACKAGES += \
     PrintSpooler \
     PrintRecommendationService \
     ProxyHandler \
-    QuickSearchBox \
     SecureElement \
     Settings \
     SettingsIntelligence \
